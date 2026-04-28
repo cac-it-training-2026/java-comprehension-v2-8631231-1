2ec1cbc55a80737600d17f2ec2d4f810b62986c6
hikaruk <hikaruk@C010423221.zipangu.cac.co.jp>
2026-04-28 16:13:14 +0900

    0428　package q01_basic.question04;まで完了
    理解できた点
    ・クラスをまたいで使用するstatica
    ・コンストラクタの引数
    
    不明点、調査中
    ・question5継承関係
    ・オーバーライド

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
@@ -4,6 +4,11 @@ public class SystemMain03 {
 
 	public static void main(String[] args) {
 		//TODO ここから実装する
+		Member member = new Member();
+		member.showMember();
+
+		Member member1 = new Member("Miura manabu", 28, 2);
+		member1.showMember();
 
 	}
 
